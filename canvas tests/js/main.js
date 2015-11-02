1be6a6171474b316d47e9b5c21afb3bd8def3b8f
kylegoines <kyle@athleticsnyc.com>
2015-11-02 16:01:34 -0500

    adding mouse hover interactivity

--- canvas tests/js/main.js
+++ canvas tests/js/main.js
@@ -38,6 +38,30 @@ window.onload = function() {
 
 	initAnimation();
 
+	hoverAction( canvas );
+
+}
+
+/////////////////////////////////////////////////////////////////////
+
+function hoverAction( elem ) {
+
+	elem.addEventListener('mousemove', function( evt ) {
+		
+		var mousePos = getMousePos( elem, evt );
+		console.log(mousePos);
+	} );
+
+}
+
+function getMousePos(canvas, evt) {
+
+	var rect = canvas.getBoundingClientRect();
+	return {
+		x: evt.clientX - rect.left,
+		y: evt.clientY - rect.top
+	};
+
 }
 
 /////////////////////////////////////////////////////////////////////
