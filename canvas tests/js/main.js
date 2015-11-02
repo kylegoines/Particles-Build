78dbc8a54e220dc4c3ac1e4deb9fa460b467b3d6
kylegoines <kyle@athleticsnyc.com>
2015-11-02 15:53:45 -0500

    cleaning and updating code

--- canvas tests/js/main.js
+++ canvas tests/js/main.js
@@ -1,56 +1,52 @@
-var imageArray      = new Array();
-var cloudsSpace = 1.8;
-var cloudsCount = 6;     
+var imageArray = [];
+var cloudsCount = 6;
 var imageLoadSet  = 0;
 var cloudsWidth = 200;
-var fps = 10;
-var now;
-var then = Date.now();
-var interval = 1000/fps;
-var delta;
+var canvas;
 
-window.onload = function() { 
+window.onload = function() {
 
-   canvasCL  = document.getElementById('canvasClouds'); 
+	canvas  = document.getElementById( 'canvasClouds' ); 
 
-   canvasHeight = $(document).height();
-   canvasWidth = $(document).width()
+	canvasHeight = $( document ).height();
+	canvasWidth = $( document ).width()
 
-   $('canvas').attr('height', $(document).height())
-   $('canvas').attr('width', $(document).width())
+	$('canvas').attr( 'height', $(document).height() );
+	$('canvas').attr( 'width', $(document).width() );
 
-   context = canvasCL.getContext('2d');
+	context = canvas.getContext('2d');
 
-   var cloud0 = new Image();
-   cloud0.src = 'img/nanit.svg';
+	var cloud0 = new Image();
+	cloud0.src = 'img/nanit.svg';
 
-   var cloud1 = new Image();
-   cloud1.src = 'img/nanit2.svg';
+	var cloud1 = new Image();
+	cloud1.src = 'img/nanit2.svg';
 
-   // D3. CLOUD image load functions.
-   cloud0.onload = function() {
-   	createCloud(0, cloud0); imageLoadSet++;
-   	createCloud(1, cloud0); imageLoadSet++;
-   	createCloud(2, cloud0); imageLoadSet++;
-   	createCloud(3, cloud0); imageLoadSet++;
-   	createCloud(4, cloud0); imageLoadSet++;
-   	createCloud(5, cloud0); imageLoadSet++;
-   }
+	cloud0.onload = function() {
+		createCloud( 0, cloud0 ); imageLoadSet++;
+		createCloud( 1, cloud0 ); imageLoadSet++;
+		createCloud( 2, cloud0 ); imageLoadSet++;
+		createCloud( 3, cloud0 ); imageLoadSet++;
+		createCloud( 4, cloud0 ); imageLoadSet++;
+		createCloud( 5, cloud0 ); imageLoadSet++;
+	}
 
-   cloud1.onload = function() {
-   	createCloud(6, cloud1); imageLoadSet++;
-   	createCloud(7, cloud1); imageLoadSet++;
-   }
+	cloud1.onload = function() {
+		createCloud( 6, cloud1 ); imageLoadSet++;
+		createCloud( 7, cloud1 ); imageLoadSet++;
+	}
 
-   initAnimation();
-   
+	initAnimation();
+
 }
 
-function negOrPos( number ){
+/////////////////////////////////////////////////////////////////////
 
-	var modifier = Math.round(Math.random())
+function negOrPos( number ) {
 
-	if ( modifier === 1) {
+	var modifier = Math.round( Math.random() )
+
+	if ( modifier === 1 ) {
 		return -(number)
 	} else {
 		return number
@@ -58,103 +54,103 @@ function negOrPos( number ){
 
 }
 
-// F. CREATE CLOUD function.
-function createCloud(number, image){
+/////////////////////////////////////////////////////////////////////
 
-	var speedNum = Math.floor(Math.random() * 4) + 1;
-	var speedNum2 = Math.floor(Math.random() * 4) + 1;
+function createCloud( number, image ) {
 
+	var speedNum = Math.floor( Math.random() * 4 ) + 1;
+	var speedNum2 = Math.floor( Math.random() * 4 ) + 1;
 
+	imageArray[number]= {};
+	imageArray[number].xPos   = ( Math.floor( Math.random() * canvasWidth ) + 1 );
+	imageArray[number].yPos   = ( Math.floor( Math.random() * canvasHeight ) + 1 );
+	imageArray[number].image  = image;
+	imageArray[number].rotate = Math.floor( Math.random() * 360 ) + 1;
+	imageArray[number].dx =  negOrPos( speedNum );
+	imageArray[number].dy = negOrPos( speedNum2 );
 
-   imageArray[number]= {};
-   imageArray[number].xPos   = (Math.floor(Math.random() * canvasWidth) + 1);
-   imageArray[number].yPos   = (Math.floor(Math.random() * canvasHeight) + 1);
-   imageArray[number].image  = image;
-   imageArray[number].rotate = Math.floor(Math.random() * 360) + 1
-   imageArray[number].dx =  negOrPos(speedNum)
-   imageArray[number].dy = negOrPos(speedNum2)
+	//normalize numbers if they dont jive well
+	if ( imageArray[number].xPos + 60 > canvasWidth ) {
+		imageArray[number].xPos = imageArray[number].xPos - 60;
+	}
 
-   //normalize numbers if they dont jive well
-   if ( imageArray[number].xPos + 150 > canvasWidth ) {
-   		imageArray[number].xPos = imageArray[number].xPos - 150;
-   }
-
-   if ( imageArray[number].yPos + 150 > canvasHeight ) {
-   		imageArray[number].yPos = imageArray[number].yPos - 150;
-   }
-
-
+	if ( imageArray[number].yPos + 60 > canvasHeight ) {
+		imageArray[number].yPos = imageArray[number].yPos - 60;
+	}
 
 }
+
+/////////////////////////////////////////////////////////////////////
 
 function initAnimation() {
 
-      ( function animLoop() {
+	( function animLoop() {
 
-        requestAnimationFrame( animLoop );
-    	
+		requestAnimationFrame( animLoop );
+
 		render();
 		update();
 
-      } )();
+	  } )();
 }
 
-
+/////////////////////////////////////////////////////////////////////
 
 function update() {
 
 	for ( var c=0; c < ( imageArray.length ); c++ ) {
 
-		if ( imageArray[c].xPos < 0 || imageArray[c].xPos > canvasWidth - 150 ) {
-            imageArray[c].dx = -imageArray[c].dx;
-        }
+		if ( imageArray[c].xPos < 0 || imageArray[c].xPos > canvasWidth - 60 ) {
+			imageArray[c].dx = -imageArray[c].dx;
+		}
 
-        if ( imageArray[c].yPos < 0 || imageArray[c].yPos > canvasHeight - 150 ) {
-            imageArray[c].dy = -imageArray[c].dy;
-        }
+		if ( imageArray[c].yPos < 0 || imageArray[c].yPos > canvasHeight - 60 ) {
+			imageArray[c].dy = -imageArray[c].dy;
+		}
 
-        imageArray[c].xPos += imageArray[c].dx;
-        imageArray[c].yPos += imageArray[c].dy;
+		imageArray[c].xPos += imageArray[c].dx;
+		imageArray[c].yPos += imageArray[c].dy;
 
 	}
 
 }
 
+/////////////////////////////////////////////////////////////////////
 
 function render() {
 
-	context.clearRect( 0, 0, canvasCL.width, canvasCL.height );
+	context.clearRect( 0, 0, canvasWidth, canvasHeight );
  
 	for ( var c=0; c < ( imageArray.length ); c++) {
 
 		if (  imageLoadSet >= cloudsCount ) {
 
-      	context.save()
+		context.save()
 
-      	imageArray[c].xPos = imageArray[c].xPos;
-      	imageArray[c].yPos = imageArray[c].yPos;
+		imageArray[c].xPos = imageArray[c].xPos;
+		imageArray[c].yPos = imageArray[c].yPos;
 
-      	//first translate to the object
-      	context.translate( imageArray[c].xPos, imageArray[c].yPos );
+		//first translate to the object
+		context.translate( imageArray[c].xPos, imageArray[c].yPos );
 
-      	//then translate further. get to the center of the object
-      	context.translate( 75, 75 );
+		//then translate further. get to the center of the object
+		context.translate( 30, 30 );
 
-      	//incriment and add rotate
-      	imageArray[c].rotate = imageArray[c].rotate + 1
+		//incriment and add rotate
+		imageArray[c].rotate = imageArray[c].rotate + 1
 
-      	//rotate the canvas
-      	context.rotate( imageArray[c].rotate* Math.PI / 180 )
+		//rotate the canvas
+		context.rotate( imageArray[c].rotate* Math.PI / 180 )
 
-      	//return the canvas to the orgional position
-      	//first image
-      	//half the center out
-      	//the negeative version of the second translate...
-		context.drawImage(imageArray[c].image, -75, -75, 150, 150 );
+		//return the canvas to the orgional position
+		//first image
+		//half the center out
+		//the negeative version of the second translate...
+		context.drawImage( imageArray[c].image, -30, -30, 60, 60 );
 
 		context.restore();
 
-      }
+	  }
    }
 
 }
